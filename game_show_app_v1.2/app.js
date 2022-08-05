a661dbbb1fb06865056bde39a9a9a0592344dc6e
MABBenton <mabbenton7@gmail.com>
2022-08-05 15:59:01 -0700

    added start game button eventListener, getRandomPhraseAsArray function

--- game_show_app_v1.2/app.js
+++ game_show_app_v1.2/app.js
@@ -1,6 +1,6 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
-const btn_reset = document.getElementsByClassName('btn_reset');
+const btn__reset = document.getElementsByClassName('btn__reset')[0];
 let missed = 0;
 
 const phrases = [
@@ -11,32 +11,36 @@ const phrases = [
   'shire'    
 ];
 
-//return a random phrase from an array
-const getRandomPhraseAsArray = arr => {
 
+// return a random phrase from an array
+const getRandomPhraseAsArray = arr => {
+  let randomNumber = Math.floor(Math.random() * phrases.length);
+  let phrase = phrases[randomNumber];
+  return phrase;
 }
+console.log(getRandomPhraseAsArray());
 
 // adds the letters of a string to the display
 const addPhraseToDisplay = arr => {
 
 }
 
-//check if a letter is in the phrase
+// check if a letter is in the phrase
 const checkLetter = button => {
 
 }
 
-//check if the game has been won or lost 
+// check if the game has been won or lost 
 const checkWin = () => {
 
 }
 
-//listen for the start game button to be pressed
-startButton.addEventListener('click', () => {
-
+// listen for the start game button to be pressed
+btn__reset.addEventListener('click', () => {
+  document.getElementById('overlay').style.display = 'none';
 });
 
-//listen for the onscreen keyboard to be clicked
-qwerty.addEventListener('click', e => {
+// listen for the onscreen keyboard to be clicked
+qwerty.addEventListener('click', (e) => {
 
 });
